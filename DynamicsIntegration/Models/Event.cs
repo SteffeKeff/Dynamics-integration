838f8e862f1073b42cefc8b9e8cfba7c01de4c3a
Osama B <osama.benhabara@ungapped.com>
2015-11-04 09:39:49 +0100

    Add Dynamics SDK

---
+++ DynamicsIntegration/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicsIntegration.Models
+{
+    public class Event
+    {
+        public string title { get; set; }
+        string description { get; set; }
+
+        int lat { get; set; }
+        int lng { get; set; }
+
+
+    }
+}
---
+++ DynamicsIntegration/Models/Events.cs
@@ -0,0 +1,13 @@
+﻿using DynamicsIntegration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicsIntegration.Models
+{
+    public class Events
+    {
+        public List<Event> events { get; set; }
+    }
+}
